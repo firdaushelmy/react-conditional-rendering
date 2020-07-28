c144ed51d338cbcc9c707a609aabd29eb0131c71
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-07-28 23:49:17 +0800

    break down existing code to conditional login function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 
-function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
+let isLoggedIn = true
+
+function renderConditionally() {
+  if (isLoggedIn === false) {
+    return <h1>Hello</h1>
+  } else {
+    return (
       <form className="form">
         <input type="text" placeholder="Username" />
         <input type="password" placeholder="Password" />
         <button type="submit">Login</button>
       </form>
+    )
+  }
+}
+
+
+function App() {
+  return (
+    <div className="container">
+      {renderConditionally()}
     </div>
   );
 }
